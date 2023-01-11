c59cc530760d8ae7ef7d5c8004d67346aaba5b35
David Sweet <dsweet@meta2>
2023-01-11 10:05:11 -0500

    ok

--- experiments/verify_lunar_linear.py
+++ experiments/verify_lunar_linear.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
 
     seed = None
     # env_conf = EnvConf("MountainCarContinuous-v0", seed=seed, max_steps=1000, solved=9999, show_frames=100, num_opt_0=1000, k_action=10)
-    env_conf = EnvConf("LunarLander-v2", seed=seed, max_steps=500, kwargs={"continuous": True}, solved=999, show_frames=30, num_opt_0=3000)
-    # env_conf = EnvConf("Ant-v4", seed=seed, max_steps=1000, solved=999, show_frames=30, num_opt_0=3000)
+    # env_conf = EnvConf("LunarLander-v2", seed=seed, max_steps=500, kwargs={"continuous": True}, solved=999, show_frames=30, num_opt_0=3000)
+    env_conf = EnvConf("Ant-v4", seed=seed, max_steps=1000, solved=999, show_frames=30, num_opt_0=3000)
 
     optimize(env_conf, num_iterations)
