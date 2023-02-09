af0528e4d3197a88b177952c281c2484eb3708ac
JuJu-Ren <jujuren.info@gmail.com>
2023-02-08 21:54:42 -0500

    updating

--- problems/branin.py
+++ problems/branin.py
@@ -9,9 +9,9 @@ class Branin:
     def __call__(self, x):
         a=1
         b=5.1/(4*np.pi**2)
-        c=5/pi
+        c=5/np.pi
         r=6
         s=10
         t=1/(8*np.pi)
-        f = a*(x[1]-b*x[0]**2+c*x[0]-r)**2+s*(1-t)*cos(x[0])+s
+        f = a*(x[1]-b*x[0]**2+c*x[0]-r)**2+s*(1-t)*np.cos(x[0])+s
         return f
