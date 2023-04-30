6c9ad9fea1e59195d7f54ab5f798b61cdc6625a2
JuJu-Ren <jujuren.info@gmail.com>
2023-04-30 12:29:41 -0400

    update

--- bat_juju.sh
+++ bat_juju.sh
@@ -5,12 +5,10 @@ export NUMEXPR_NUM_THREADS=3
 export OMP_NUM_THREADS=3
 
 DDIR=exp_1_n
-for TAG in  f:ackley-2d f:ackley-10d f:ackley-30d f:sphere-2d f:sphere-10d f:sphere-30d f:branin-2d f:branin-10d f:branin-30d 
-# f:stybtang-2d f:rosenbrock-2d f:dixonprice-2d f:griewank-2d f:levy-2d f:michalewicz-2d f:rastrigin-2d f:beale-2d f:bukin-2d f:crossintray-2d f:dropwave-2d f:eggholder-2d f:hartmann-2d f:holdertable-2d f:shubert-2d f:shekel-2d f:sixhumpcamel-2d f:threehumpcamel-2d f:crossintray-2d f:grlee12-2d f:hartmann-3d f:hartmann-4d f:hartmann-6d
+for TAG in f:sphere-2d f:sphere-10d f:sphere-30d f:levy-2d f:levy-10d f:levy-30d f:stybtang-2d f:stybtang-10d f:stybtang-30d f:rosenbrock-2d f:rosenbrock-10d f:rosenbrock-30d f:dixonprice-2d f:dixonprice-10d f:dixonprice-30d f:griewank-2d f:griewank-10d f:griewank-30d f:michalewicz-2d f:michalewicz-10d f:michalewicz-30d f:rastrigin-2d f:rastrigin-10d f:rastrigin-30d f:beale-2d f:bukin-2d f:crossintray-2d f:dropwave-2d f:eggholder-2d f:hartmann-2d f:holdertable-2d f:shubert-2d f:shekel-4d f:sixhumpcamel-2d f:threehumpcamel-2d f:grlee12-1d f:hartmann-3d f:hartmann-4d f:hartmann-6d
 do 
-# random sobol ioptv_ei ax ei 
- for TTYPE in random sobol ieig sobol_mes sobol_gibbon iopt_ei
- # sobol ei pes mes sobel_ei iopt
+#  random sobol ieig sobol_mes sobol_gibbon iopt_ei
+ for TTYPE in random sobol pes ieig_znm ieig_cem ieig_ts cem_ts sr
  # for TTYPE in variance maximin maximin-toroidal iopt
  do
      echo $TAG $TTYPE
