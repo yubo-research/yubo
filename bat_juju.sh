510c9ed548e2a0ae2d7d4af1359b097f122cb208
JuJu-Ren <jujuren.info@gmail.com>
2023-08-03 16:59:54 -0400

    figures

--- bat_juju.sh
+++ bat_juju.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-
+cd /home/juju/projects/bbo
+export PYTHONPATH=/home/juju/projects/bbo
 export MKL_NUM_THREADS=3
 export NUMEXPR_NUM_THREADS=3
 export OMP_NUM_THREADS=3
 
-DDIR=exp_1_n
-for TAG in f:ackley-2d 
+for TAG in f:branin-2d 
 # f:sphere-10d f:sphere-30d f:levy-2d f:levy-10d f:levy-30d f:stybtang-2d f:stybtang-10d f:stybtang-30d f:rosenbrock-2d f:rosenbrock-10d f:rosenbrock-30d f:dixonprice-2d f:dixonprice-10d f:dixonprice-30d f:griewank-2d f:griewank-10d f:griewank-30d f:michalewicz-2d f:michalewicz-10d f:michalewicz-30d f:rastrigin-2d f:rastrigin-10d f:rastrigin-30d f:beale-2d f:bukin-2d f:crossintray-2d f:dropwave-2d f:eggholder-2d f:hartmann-2d f:holdertable-2d f:shubert-2d f:shekel-4d f:sixhumpcamel-2d f:threehumpcamel-2d f:grlee12-1d f:hartmann-3d f:hartmann-4d f:hartmann-6d
 do 
 #  random sobol ieig sobol_mes sobol_gibbon iopt_ei pes ieig_znm ieig_cem ieig_ts cem_ts sr
- for TTYPE in sobol
+ for TTYPE in random sobol
  # for TTYPE in variance maximin maximin-toroidal iopt
  do
-     echo $TAG $TTYPE
+     echo "Start" $TAG $TTYPE
      # mkdir -p results/${DDIR}/${TAG}
-     python experiments/test.py ${TAG} ${TTYPE} 
+     python experiments/run_test1.py ${TAG} ${TTYPE} >/dev/null 2>&1
+     echo "Finish"
  done
 done
