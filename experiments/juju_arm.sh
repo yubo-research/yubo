b7ca696df1c0af3960353f003de22e0c4f476f0a
JuJu-Ren <jujuren.info@gmail.com>
2023-08-28 10:55:46 -0400

    update

--- experiments/juju_arm.sh
+++ experiments/juju_arm.sh
@@ -9,7 +9,7 @@ for NUM_SAMPLES in 30
     do
     for ITER in 3
         do
-        for Q_NUM in 1 3 10 30 100
+        for Q_NUM in 100
             do
             for TAG in f:ackley-2d f:ackley-10d
             # f:branin-2d f:ackley-2d f:sphere-30d f:levy-2d f:levy-10d f:levy-30d f:stybtang-2d f:stybtang-10d f:stybtang-30d f:rosenbrock-2d f:rosenbrock-10d f:rosenbrock-30d f:dixonprice-2d f:dixonprice-10d f:dixonprice-30d f:griewank-2d f:griewank-10d f:griewank-30d f:michalewicz-2d f:michalewicz-10d f:michalewicz-30d f:rastrigin-2d f:rastrigin-10d f:rastrigin-30d f:beale-2d f:bukin-2d f:crossintray-2d f:dropwave-2d f:eggholder-2d f:hartmann-2d f:holdertable-2d f:shubert-2d f:shekel-4d f:sixhumpcamel-2d f:threehumpcamel-2d f:grlee12-1d f:hartmann-3d f:hartmann-4d f:hartmann-6d
