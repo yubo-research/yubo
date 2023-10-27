ae5dc1de53b7f95740e98d5a7a9786cd9d65b71c
JuJu-Ren <jujuren.info@gmail.com>
2023-10-27 08:39:35 -0400

    update

--- experiments/juju_arm.sh
+++ experiments/juju_arm.sh
@@ -9,24 +9,27 @@ for NUM_SAMPLES in 30
     do
     for ITER in 1
         do
-        for Q_NUM in 2 3 10 30 100
+        for Q_NUM in 2
             do
-            for TAG in  f:ackley-1d f:dixonprice-1d f:griewank-1d f:levy-1d f:rastrigin-1d f:sphere-1d f:stybtang-1d 
-            # f:michalewicz-100d f:rosenbrock-100d
-            # f:branin-2d f:ackley-2d f:sphere-30d f:levy-2d f:levy-10d f:levy-30d f:stybtang-2d f:stybtang-10d f:stybtang-30d f:rosenbrock-2d f:rosenbrock-10d f:rosenbrock-30d f:dixonprice-2d f:dixonprice-10d f:dixonprice-30d f:griewank-2d f:griewank-10d f:griewank-30d f:michalewicz-2d f:michalewicz-10d f:michalewicz-30d f:rastrigin-2d f:rastrigin-10d f:rastrigin-30d f:beale-2d f:bukin-2d f:crossintray-2d f:dropwave-2d f:eggholder-2d f:hartmann-2d f:holdertable-2d f:shubert-2d f:shekel-4d f:sixhumpcamel-2d f:threehumpcamel-2d f:grlee12-1d f:hartmann-3d f:hartmann-4d f:hartmann-6d
-                do 
-                #  random sobol ieig sobol_mes sobol_gibbon iopt_ei pes ieig_znm ieig_cem ieig_ts cem_ts sr
-                for TTYPE in mtv sobol
-                    # for TTYPE in variance maximin maximin-toroidal iopt
-                    do
-                        for FOLDER in figure_test/${Q_NUM}arm/1d
+            for dim in 100
+                do
+                for TAG in f:ackley-${dim}d
+                # f:ackley-${dim}d f:dixonprice-${dim}d f:griewank-${dim}d f:levy-${dim}d f:rastrigin-${dim}d f:sphere-${dim}d f:stybtang-${dim}d f:michalewicz-${dim}d f:rosenbrock-${dim}d
+                # f:branin-2d f:ackley-2d f:sphere-30d f:levy-2d f:levy-10d f:levy-30d f:stybtang-2d f:stybtang-10d f:stybtang-30d f:rosenbrock-2d f:rosenbrock-10d f:rosenbrock-30d f:dixonprice-2d f:dixonprice-10d f:dixonprice-30d f:griewank-2d f:griewank-10d f:griewank-30d f:michalewicz-2d f:michalewicz-10d f:michalewicz-30d f:rastrigin-2d f:rastrigin-10d f:rastrigin-30d f:beale-2d f:bukin-2d f:crossintray-2d f:dropwave-2d f:eggholder-2d f:hartmann-2d f:holdertable-2d f:shubert-2d f:shekel-4d f:sixhumpcamel-2d f:threehumpcamel-2d f:grlee12-1d f:hartmann-3d f:hartmann-4d f:hartmann-6d
+                    do 
+                    #  random sobol ieig sobol_mes sobol_gibbon iopt_ei pes ieig_znm ieig_cem ieig_ts cem_ts sr
+                    for TTYPE in random
+                        # for TTYPE in variance maximin maximin-toroidal iopt
                         do
-                            echo "Start" $TAG $TTYPE $Q_NUM "arms" $ITER "Iters" $FOLDER
-                            # mkdir -p results/${DDIR}/${TAG}
-
-                            python experiments/exp_2_arm.py ${TAG} ${TTYPE} ${Q_NUM} ${NUM_SAMPLES} ${ITER} ${FOLDER}
-                            #>/dev/null 2>&1
-                            echo "Finish"
+                            for FOLDER in figure_test/${Q_NUM}arm/${dim}d
+                            do
+                                echo "Start" $TAG $TTYPE $Q_NUM "arms" $ITER "Iters" $FOLDER
+                                # mkdir -p results/${DDIR}/${TAG}
+    
+                                python experiments/exp_2_arm.py ${TAG} ${TTYPE} ${Q_NUM} ${NUM_SAMPLES} ${ITER} ${FOLDER}
+                                #>/dev/null 2>&1
+                                echo "Finish"
+                            done
                         done
                     done
                 done
