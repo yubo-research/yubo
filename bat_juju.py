7c404c3fb53c85c0cf370711b794d0a00ff40130
JuJu-Ren <jujuren.info@gmail.com>
2023-11-02 21:49:01 -0400

    update

--- bat_juju.py
+++ bat_juju.py
@@ -30,7 +30,7 @@ def prep_cmds(ddir, funcs, dims, num_arms, num_samples, opts):
                 problem = f"f:{func}-{dim}d"
                 out_dir = f"results/{ddir}/{problem}"
                 os.makedirs(out_dir, exist_ok=True)
-                cmd = f"python experiments/exp_2.py {problem} {opt} {num_arms} {num_samples} > {out_dir}/{opt} 2>&1"
+                cmd = f"python experiments/exp_2.py {problem} {opt} {num_arms} 30 {num_samples} > {out_dir}/{opt} 2>&1"
                 cmds.append(cmd)
     return cmds
 
@@ -51,20 +51,49 @@ if __name__=="__main__":
 
     # opts=["sobol", "sobol_c", "ei", "ucb", "ei_c", "mcmc_ts", "mtav_ei", "mtav_ts", "mtav_ucb", "ucb_c"],
 
-    opts = ["mtv_then_ei", "mtv_then_ucb"]
+    opts = ["sobol", "random","mtv"]
     
+    # cmds = prep_cmds(
+    #     ddir="exp_2_plots/plot_1d_100arm",
+    #     funcs=funcs_1d,
+    #     dims=[1],
+    #     num_arms=100,
+    #     num_samples=1,
+    #     opts=opts,
+    # ) + prep_cmds(
+    #     ddir="exp_2_plots/plot_2d_100arm",
+    #     funcs=funcs_10d,
+    #     dims=[2],
+    #     num_arms=100,
+    #     num_samples=1,
+    #     opts=opts,
+    # )+ prep_cmds(
+    #     ddir="exp_2_plots/plot_3d_100arm",
+    #     funcs=funcs_10d,
+    #     dims=[3],
+    #     num_arms=100,
+    #     num_samples=1,
+    #     opts=opts,
+    # )+ 
     cmds = prep_cmds(
-        ddir="exp_2_mtv_1d",
-        funcs=funcs_1d,
-        dims=[1],
-        num_arms=4,
-        num_samples=1,
-        opts=opts,
-    ) + prep_cmds(
-        ddir="exp_2_mtv_10d",
+        ddir="exp_2_plots/plot_10d_100arm",
         funcs=funcs_10d,
         dims=[10],
-        num_arms=10,
+        num_arms=100,
+        num_samples=1,
+        opts=opts,
+    )+ prep_cmds(
+        ddir="exp_2_plots/plot_30d_100arm",
+        funcs=funcs_10d,
+        dims=[30],
+        num_arms=100,
+        num_samples=1,
+        opts=opts,
+    )+ prep_cmds(
+        ddir="exp_2_plots/plot_100d_100arm",
+        funcs=funcs_10d,
+        dims=[100],
+        num_arms=100,
         num_samples=1,
         opts=opts,
     )
