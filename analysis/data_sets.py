d853eb7f142c046b983452063c76256abb077b90
JuJu-Ren <jujuren.info@gmail.com>
2023-11-10 22:42:55 -0500

    update

--- analysis/data_sets.py
+++ analysis/data_sets.py
@@ -1,15 +1,14 @@
 import os
 
 import numpy as np
-from scipy.stats import rankdata
 
 
 def problems_in(exp_tag):
-    return sorted(os.listdir(f"results/{exp_tag}"))
+    return sorted(os.listdir(f"/Users/dsweet2/Projects/bbo/results/{exp_tag}"))
 
 
 def optimizers_in(exp_tag, problem):
-    return sorted(os.listdir(f"results/{exp_tag}/{problem}"))
+    return sorted(os.listdir(f"/Users/dsweet2/Projects/bbo/results/{exp_tag}/{problem}"))
 
 
 def all_in(exp_tag):
@@ -71,6 +70,8 @@ def load_kv(fn, keys, grep_for=None):
 
 def load_traces(fn, key="return"):
     o = load_kv(fn, ["i_sample", key], grep_for="TRACE:")
+    if len(o) == 0:
+        return None
     traces = []
     n_x = None
     for i_sample in np.unique(o["i_sample"]):
@@ -86,130 +87,61 @@ def load_traces(fn, key="return"):
     return traces
 
 
-def summarize_traces(traces):
-    """
-    One problem, one optimizer, multiple traces
-    """
-    mu = traces.mean(axis=0)
-    sd = traces.std(axis=0)
-    se = sd / np.sqrt(len(traces))
-    return mu, se
-
-
-def normalize_summaries(summaries: dict, i_only=None):
-    """
-    One problem, multiple optimizers
-
-    summaries[optimizer_name] = (mu, se)
+def load_multiple_traces(data_locator, exp_tag, problem_names, opt_names):
+    import numpy.ma as npma
 
     """
-
-    if i_only is not None:
-        summaries = {k: ([mu[i_only]], [se[i_only]]) for k, (mu, se) in summaries.items()}
-
-    all_mu = np.array([s[0] for s in summaries.values()])
-    mean = all_mu.mean()
-    std = all_mu.std()
-    print("total mean", mean, "total std", std)
-    return {optimizer_name: ((mu - mean) / std, se / std) for optimizer_name, (mu, se) in summaries.items()}
-
-
-def aggregate_normalized_summaries(normalized_summaries: dict):
+    Returns traces[i_problem, i_opt, i_replication, i_round]
     """
-    Aggregate over problems.
-    normalized_summaries[problem_name][optimizer_name] = (normed_mu, normed_se)
-    """
-    agg_by_opt = {}
-    for problem_name, ns_by_opt in normalized_summaries.items():
-        for optimizer_name, (mu, se) in ns_by_opt.items():
-            if optimizer_name not in agg_by_opt:
-                agg_by_opt[optimizer_name] = []
-            agg_by_opt[optimizer_name].append((mu, se))
 
-    summarized_agg_by_opt = {}
-    for optimizer_name, agg in agg_by_opt.items():
-        agg = np.array(agg)
-        mu = agg[:, 0].mean(axis=0)
-        se = np.sqrt((agg[:, 1] ** 2).mean(axis=0))
-        summarized_agg_by_opt[optimizer_name] = (mu, se)
-    return summarized_agg_by_opt
+    num_bad = 0
+    num_tot = 0
 
+    def _report_bad(problem_name, opt_name):
+        nonlocal num_bad
+        print("BAD:", exp_tag, problem_name, opt_name)
+        num_bad += 1
 
-def load_as_normalized_summaries(exp_tag, problem_names, optimizer_names, data_locator, i_only=None):
-    normalized_summaries = {}
-    count = {}
-    for problem_name in problem_names:
-        summaries = {}
-        for optimizer_name in optimizer_names:
-            fn = data_locator(exp_tag, problem_name, optimizer_name)
+    traces = None
+    for i_problem, problem_name in enumerate(problem_names):
+        for i_opt, opt_name in enumerate(opt_names):
+            num_tot += 1
             try:
-                traces = load_traces(fn)
-            except Exception as e:
-                print(f"Could not load {fn}", e)
+                trace = load_traces(data_locator(exp_tag, problem_name, opt_name))
+            except FileNotFoundError:
+                _report_bad(problem_name, opt_name)
                 continue
-            count[f"{problem_name}-{optimizer_name}"] = len(traces)
-            summaries[optimizer_name] = summarize_traces(traces)
-        print("problem", problem_name)
-        normalized_summaries[problem_name] = normalize_summaries(summaries, i_only)
+            if trace is None:
+                _report_bad(problem_name, opt_name)
+                continue
+            if traces is None:
+                traces = np.nan * np.ones(shape=(len(problem_names), len(opt_names), trace.shape[0], trace.shape[1]))
+            if trace.shape != traces[i_problem, i_opt, ...].shape:
+                _report_bad(problem_name, opt_name)
+                continue
+            traces[i_problem, i_opt, ...] = trace
 
-    med_count = np.median(list(count.values()))
-    for k, c in count.items():
-        if c != med_count:
-            print(f"Odd count {c} != {med_count} for {k}")
-
-    return normalized_summaries
-
-
-def agg_rank_summaries(exp_tag, problems, optimizers, data_locator, num_boot=100):
-    summaries_by_opt = {}
-    for problem in problems:
-        summary = load_rank_summary(exp_tag, problem, optimizers, data_locator, num_boot)
-        for optimizer in summary:
-            if optimizer not in summaries_by_opt:
-                summaries_by_opt[optimizer] = []
-            summaries_by_opt[optimizer].append(summary[optimizer])
-
-    summary = {}
-    for optimizer, mus_ses in summaries_by_opt.items():
-        mus_ses = np.array(mus_ses)
-        mus = mus_ses[:, 0]
-        ses = mus_ses[:, 0]
-        se = 1 / (1 / ses**2).sum()
-        mu = se * ((1 / ses**2) * mus).sum()
-        summary[optimizer] = (mu, se)
-
-    return summary
+    traces = npma.masked_invalid(traces)
+    if num_bad > 0:
+        print(f"\n{num_bad} / {num_tot} files bad. {100*traces.mask.mean():.1f}% missing data")
+    else:
+        print("No bad data")
+    return npma.masked_invalid(traces)
 
 
-def load_rank_summary(exp_tag, problem, optimizers, data_locator, num_boot=100):
-    finals = []
-    for optimizer in optimizers:
-        # traces ~ num_replicates X num_rounds
-        try:
-            traces = load_traces(data_locator(exp_tag, problem, optimizer))
-        except Exception as e:
-            print(f"Could not load {exp_tag} {problem} {optimizer}", e)
-            continue
-        finals.append(traces[:, -1])
-    mus_ses = boot_ranks(finals, num_boot)
-    return dict(zip(optimizers, mus_ses))
+def range_summarize(traces: np.ndarray):
+    """
+    traces[i_problem, i_opt, i_replication, i_round]
+    Returns mu,se ~ [i_problem, i_opt]
+    """
+    y_min = traces.min(axis=1, keepdims=True)
+    y_max = traces.max(axis=1, keepdims=True)
+    z = (traces - y_min) / (y_max - y_min)
+    z = z[..., -1]
 
+    z = z.swapaxes(0, 1)
+    z = z.reshape(z.shape[0], z.shape[1] * z.shape[2])
+    mu = z.mean(axis=-1)
+    se = z.std(axis=-1) / np.sqrt(z.shape[-1])
 
-def _boot_sample(finals):
-    bs = []
-    for j in range(len(finals)):
-        bs.append(np.random.choice(finals[j]))
-    return bs
-
-
-def boot_ranks(finals, num_boot):
-    # finals ~ [np.array([final_values])]
-    boot_samples = []
-    for _ in range(num_boot):
-        # draw a final value for each optimizer
-        ranks = rankdata(_boot_sample(finals))
-        boot_samples.append(ranks)
-    boot_samples = np.array(boot_samples)
-    mus = boot_samples.mean(axis=0)
-    ses = boot_samples.std(axis=0)
-    return list(zip(mus, ses))
+    return mu, se
