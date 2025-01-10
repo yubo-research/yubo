bb901ea3e79234e8f10a669d36ae164c9af7a70d
David Sweet <dsweet@andamooka.org>
2025-01-10 07:21:43 -0500

    ok

--- acq/fit_gp.py
+++ acq/fit_gp.py
@@ -71,7 +71,7 @@ def fit_gp_XY(X, Y, model_type=None):
     for i_try in range(3):
         mll.to(X)
         try:
-            fit_gpytorch_mll(mll)
+            fit_gpytorch_mll(mll)  # go faster: , optimizer_kwargs={"options": {"maxiter": 10}})
         except (RuntimeError, ModelFittingError) as e:
             m = e
             print(f"Retrying fit i_try = {i_try}")
