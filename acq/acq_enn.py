6b6ec5ef6c7cb19275cf0a60dbddb79cea84d0c3
David Sweet <dsweet@andamooka.org>
2025-06-29 15:18:30 -0400

    ok

--- acq/acq_enn.py
+++ acq/acq_enn.py
@@ -61,7 +61,8 @@ class AcqENN:
     def keep_top_n(self, num_keep):
         if len(self._x_train) <= num_keep:
             return self._x_train, self._y_train
-        i = self._i_pareto_fronts_strict(self._x_train, num_keep, exclude_nearest=True)
+        # TODO: Swtich to lowest discrepancy points (opposit of pivot point seletor)
+        i = self._i_pareto_fronts_discrepancy(self._x_train, num_keep)
         return self._x_train[i], self._y_train[i]
 
     def _sample_segments(self, x_0, x_far):
@@ -100,6 +101,37 @@ class AcqENN:
 
         i = np.random.choice(np.arange(len(x_front)), size=num_pivot, replace=True)
         return x_front[i]
+
+    def _i_pareto_fronts_discrepancy(self, x_cand, num_arms):
+        # Full fronts, then random selection from *last* front.
+        assert self._config.num_over_sample_per_arm == 1, self._config.num_over_sample_per_arm
+        mvn = self._enn.posterior(x_cand, exclude_nearest=True)
+        discrep = np.abs(self._y_train - mvn.mu)
+
+        i = np.argsort(-discrep, axis=0).flatten()
+        x_cand = x_cand[i]
+        se = mvn.se[i]
+
+        i_all = list(range(len(se)))
+        i_keep = []
+
+        num_tries = 0
+        while len(i_keep) < num_arms:
+            num_tries += 1
+            assert num_tries < 100, num_tries
+            se_max = -1e99
+            i_front = []
+            for i in i_all:
+                if se[i] >= se_max:
+                    i_front.append(i)
+                    se_max = se[i]
+            if len(i_keep) + len(i_front) <= num_arms:
+                i_keep.extend(i_front)
+            else:
+                i_keep.extend(np.random.choice(i_front, size=num_arms - len(i_keep), replace=False))
+            i_all = sorted(set(i_all) - set(i_front))
+
+        return np.array(i_keep)
 
     def _i_pareto_fronts_strict(self, x_cand, num_arms, exclude_nearest=False):
         # Full fronts, then random selection from *last* front.
