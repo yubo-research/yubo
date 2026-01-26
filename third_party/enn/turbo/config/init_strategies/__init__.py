e0c0fe55968cc3fd5395f00d3e66f76747bf72f8
David Sweet <dsweet@andamooka.org>
2026-01-26 07:16:20 -0500

    ok

---
+++ third_party/enn/turbo/config/init_strategies/__init__.py
@@ -0,0 +1,9 @@
+from .hybrid_init import HybridInit
+from .init_strategy import InitStrategy
+from .lhd_only_init import LHDOnlyInit
+
+__all__ = [
+    "HybridInit",
+    "InitStrategy",
+    "LHDOnlyInit",
+]
