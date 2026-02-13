95cf9decde15bcc4251a01de06c79156b98d2b56
David Sweet <github@andamooka.org>
2026-02-12 20:58:36 -0500

    Dsweet/tweak (#8)
    * ok
    
    * ok
    
    ---------
    
    Co-authored-by: David Sweet <dsweet@andamooka.org>

---
+++ admin/install-macos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+micromamba env create -n yubo -f admin/conda-macos.yml
+micromamba activate yubo
+pip install -r requirements.txt
+
+ENV_LIB="${CONDA_PREFIX}/lib" LDFLAGS="-L${CONDA_PREFIX}/lib" LIBRARY_PATH="${CONDA_PREFIX}/lib" DYLD_LIBRARY_PATH="${CONDA_PREFIX}/lib" CPATH=$(python -c 'import pybind11; print(pybind11.get_include())') pip install --no-build-isolation "git+https://github.com/feji3769/VecchiaBO.git#subdirectory=code"
+
+pip install "LassoBench @ git+https://github.com/ksehic/LassoBench.git" --no-deps
+
+pip install ennbo --no-deps
+cargo install kiss-ai --version 0.2.5
